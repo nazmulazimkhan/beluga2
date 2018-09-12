749f5d52edb3a11b9a921ff3c63b095d57cc970d
nazmulazimkhan <nazmul.azim.khan@gmail.com>
2018-09-12 16:15:56 +1000

    fsr

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ console.log("nah");
 map.on('load', function() {
     map.addSource('BelugaGen_HobSunFsr', {
                     'type': 'geojson',
-                    'data': './GeoJSON/BelugaGen_HobSunFsr.GeoJSON'
+                    'data': './GeoJSON/BelugaGen_Fsr.geojson'
                 })
 console.log("meh");
 
@@ -45,44 +45,44 @@ console.log("meh");
             }
     });
 console.log("k");
-// //LIST OF LayerIDs and Display Text
-// var toggleableLayerIds = ['buildingEnvelopes'];
-// var toggleableLayerText = ['envelopes'];
+//LIST OF LayerIDs and Display Text
+var toggleableLayerIds = ['buildingEnvelopes'];
+var toggleableLayerText = ['envelopes'];
 
-// //TOGGLE
-// for (var i = 0; i < toggleableLayerIds.length; i++) {
-//     var id = toggleableLayerIds[i];
-//     var idText= toggleableLayerText[i];
-//     var link = document.createElement('a');
-//     link.href = '#';
-//     link.className = 'active';
-//     link.textContent = idText;
-//     link.textId = id;
+//TOGGLE
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+    var idText= toggleableLayerText[i];
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = idText;
+    link.textId = id;
 
-//     link.onclick = function (e) {
-//         var clickedLayer = this.textId;
-//         e.preventDefault();
-//         e.stopPropagation();
+    link.onclick = function (e) {
+        var clickedLayer = this.textId;
+        e.preventDefault();
+        e.stopPropagation();
 
-//         var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
 
-//         if (visibility === 'visible') {
-//             map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-//             this.className = '';
-//         } else {
-//             this.className = 'active';
-//             map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-//         }
-//     };
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
 
-//     var layers = document.getElementById('menu');
-//     layers.appendChild(link);
-// }
-
-
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
 
 
-});
+
+
+});''
 
 
 console.log("alright");
